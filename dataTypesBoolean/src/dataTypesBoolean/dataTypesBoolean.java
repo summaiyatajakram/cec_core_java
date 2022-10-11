f715ca1036ec500c455e33b10c18136e50eaaad6
summaiyatajakram <71880631+summaiyatajakram@users.noreply.github.com>
2022-10-11 10:45:06 +0530

    Add files via upload

---
+++ dataTypesBoolean/src/dataTypesBoolean/dataTypesBoolean.java
@@ -0,0 +1,13 @@
+package dataTypesBoolean;
+
+public class dataTypesBoolean {
+	     public static void main(String[] args) {
+	        boolean var = false;
+	        System.out.println(var);
+	        char var1 = '\u00A7';
+	        double d=34.45d;
+	        System.out.println(d);
+
+	    }
+	}
+
