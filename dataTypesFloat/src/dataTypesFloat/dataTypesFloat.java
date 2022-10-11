cee254485b19a88a1f8dfc4da89f30c2e427fbc3
summaiyatajakram <71880631+summaiyatajakram@users.noreply.github.com>
2022-10-11 10:38:15 +0530

    Add files via upload

---
+++ dataTypesFloat/src/dataTypesFloat/dataTypesFloat.java
@@ -0,0 +1,17 @@
+package dataTypesFloat;
+
+public class dataTypesFloat {
+	    public static void main(String[] args){
+	        int val =9/2;
+	        float val2 = 10f/6f;
+	        double val3 = 10d/6d;
+	        System.out.println("value1="+val);
+	        System.out.println("value2="+val2);
+	        System.out.println("value3="+val3);
+	        int marker = 512;
+	        double perc = marker*0.46f;
+	        System.out.println("percentage="+perc);
+	    }
+	}
+
+
