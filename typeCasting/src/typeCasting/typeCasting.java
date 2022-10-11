19171dd02323e148090122b23e628cdd6816b15c
summaiyatajakram <71880631+summaiyatajakram@users.noreply.github.com>
2022-10-11 10:50:07 +0530

    Add files via upload

---
+++ typeCasting/src/typeCasting/typeCasting.java
@@ -0,0 +1,17 @@
+package typeCasting;
+
+public class typeCasting {
+	    public static void main(String[] args) {
+
+	        float f =1.2f;
+	        long l = (long)f;
+	        System.out.println(l);
+
+	        byte b = 58;
+	        short s = b;
+	        System.out.println(s);
+	        
+	    }
+	}
+
+
